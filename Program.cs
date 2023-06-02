048792e6f90de52f2870310bbff0ae999e2a45af
dreamy-timmy <120133207+dreamy-timmy@users.noreply.github.com>
2023-06-02 17:40:17 +0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,160 @@
+// 2) Задачка для стека:
+// на вход подаётся строка, в которой могут присутствовать скобки 3-х типов: ),],}
+// Необходимо определить правильно ли расставлены скобки?(чтобы внутренние скобки закрывались раньше внешних)
+string t = "(ba{{a}f}a)";
+
+Stack<char> st = new Stack<char>();
+char[] opening = { '(', '[', '{'};
+char[] closing = { ')', ']', '}' };
+
+    // || (!opening.Contains(t[^1]) && !closing.Contains(t[^1]))
+while (closing.Contains(t[^1]) || (!opening.Contains(t[^1]) && !closing.Contains(t[^1])))
+{ 
+    if (closing.Contains(t[^1])) st.Push(t[^1]);
+    t = t[..^1]; 
+}
+var f = true;
+Console.WriteLine(t+" "+st.Peek());
+if (st.Count != 0)
+{
+    while (t != "")
+    {
+        if (Array.IndexOf(opening, t[^1]) == Array.IndexOf(closing, st.Peek()) ||
+            (!opening.Contains(t[^1]) && !closing.Contains(t[^1])))
+        {
+            if (Array.IndexOf(opening, t[^1]) == Array.IndexOf(closing, st.Peek())) st.Pop();
+            t = t[..^1];
+            
+        }
+        else
+        {
+            Console.WriteLine(t[^1]);
+            f = false;
+            break;
+        }
+    }
+
+}
+else f = false;
+
+if (f == true) Console.WriteLine("Всё расставлено правильно");
+else Console.WriteLine("Неправильно!");
+
+// с использованием делегата реализовать лямбда-выражеение для нахождения 
+//  мин, макс, суммы произведения, среднего арифм 3 чисел (1 делегат)
+
+Operation mx = (x, y, z) => Math.Max(Math.Max(x, y),z);
+Operation mn = (x, y, z) => Math.Min(Math.Min(x, y),z);
+Operation avg = (x, y, z) => (x+y+z)/3;
+Console.WriteLine(mx(1,2,3));
+Console.WriteLine(mn(1,2,3));
+Console.WriteLine(avg(1,2,3));
+
+delegate int Operation(int x, int y, int z);
+
+
+
+// три таблицы (id продукта, наименование), (id произв, наим. произв.), (id товара, id производителя, дата, кол-во)
+// группировка по дате ()
+// группировка по товару (товар, кто поставлял)
+// группировка по производителю ()
+
+Dictionary<int, string> products = new()
+{
+    [1] = "Молоко",
+    [2] = "Сыр",
+    [3] = "Колбаса",
+    [4] = "Хлеб",
+    [5] = "Яблоки",
+    [6] = "Бананы"
+};
+Dictionary<int, string> manufacturers = new()
+{
+    [1] = "Молочное производство",
+    [2] = "Мясокомбинат",
+    [3] = "Хлебная мануфактура",
+    [4] = "Фруктляндия"
+};
+List<Supply> supplies = new()
+{
+    new Supply(1, 1, new DateOnly(2022, 12, 12), 12),
+    new Supply(2, 1, new DateOnly(2022, 12, 15), 6),
+    new Supply(3, 2, new DateOnly(2022, 12, 13), 10),
+    new Supply(4, 3, new DateOnly(2022, 12, 12), 24),
+    new Supply(5, 4, new DateOnly(2022, 12, 5), 100),
+    new Supply(6, 4, new DateOnly(2022, 12, 13), 24),
+    new Supply(1, 1, new DateOnly(2022, 12, 18), 22),
+    new Supply(3, 2, new DateOnly(2022, 12, 31), 11),
+};
+
+var byDate = from supply in supplies
+             group supply by supply.Date;
+Console.WriteLine("---------------------------------------------------------------------------");
+Console.WriteLine("ПО ДАТЕ");
+Console.WriteLine("---------------------------------------------------------------------------");
+foreach (var date in byDate)
+{
+    Console.WriteLine(date.Key);
+    foreach (var supply in date)
+    {
+        Console.WriteLine($"Товар: {products[supply.ProductID]}, Производитель: {manufacturers[supply.ManufID]}, Кол-во: {supply.Amount}");
+    }
+    Console.WriteLine();
+}
+var byProduct = from supply in supplies
+                group supply by supply.ProductID;
+Console.WriteLine("---------------------------------------------------------------------------");
+Console.WriteLine("ПО ТОВАРУ");
+Console.WriteLine("---------------------------------------------------------------------------");
+foreach (var product in byProduct)
+{
+    Console.WriteLine(products[product.Key]);
+    foreach (var supply in product)
+    {
+        Console.WriteLine($"Производитель: {manufacturers[supply.ManufID]}, Кол-во: {supply.Amount}, Дата: {supply.Date}");
+    }
+    Console.WriteLine();
+}
+var byManufacturer = from supply in supplies
+                     group supply by supply.ManufID;
+Console.WriteLine("---------------------------------------------------------------------------");
+Console.WriteLine("ПО ПРОИЗВОДИТЕЛЮ");
+Console.WriteLine("---------------------------------------------------------------------------");
+foreach (var manufacturer in byManufacturer)
+{
+    Console.WriteLine(manufacturers[manufacturer.Key]);
+    foreach (var supply in manufacturer)
+    {
+        Console.WriteLine($"Товар: {products[supply.ProductID]}, Кол-во: {supply.Amount}, Дата: {supply.Date}");
+    }
+    Console.WriteLine();
+}
+/*
+var owners = from car in carsToOwners
+             group car by car.OwnerName;
+foreach (var owner in owners)
+{
+    Console.WriteLine(owner.Key);
+    foreach (var car in owner)
+    {
+        Console.WriteLine($"ID машины: {car.ID}");
+    }
+}*/
+class Supply
+{
+    public int ProductID { get; set; }
+    public int ManufID { get; set; }
+    public DateOnly Date { get; set; }
+    public int Amount { get; set; }
+    public Supply(int productID, int manufacturerID, DateOnly date, int amount) 
+    { ProductID = productID; ManufID = manufacturerID; Date = date; Amount = amount;}
+}
+
+
+
+
+
+
 //Два класса, описание машин, в классе машин, поля: id, colour, mark.
 //2-ой класс: id машины, владельцы: ФИО.
 //С использованием запросов необходимо выполнить отбор машин по опред марке, 
